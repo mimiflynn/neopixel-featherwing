cd646bd7ce4d34301dedbde1088ae2097df54d79
Mimi Flynn <mimiflynn@gmail.com>
2019-05-05 12:04:33 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import board
+import neopixel
+import time
+
+# Colors
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+PINK = (255, 100, 120)
+ORANGE = (255, 100, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+PURPLE = (255, 0, 255)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (80, 200, 175)
+WHITE = (255, 255, 255)
+
+colors = [PINK, RED, ORANGE, YELLOW, GREEN,
+          CYAN, PURPLE, BLUE, LIGHT_BLUE, WHITE]
+
+# NeoPixel setup
+pixels = neopixel.NeoPixel(board.D6, 32, brightness=.01)
+
+print('hello world')
+
+while True:
+    for x in range(8):
+        for i in range(4):    
+            col = x
+            d = i * 8
+            e = d + col
+            pixels[e] = RED
+            time.sleep(.05)
+        pixels.fill(BLACK)
+        time.sleep(.05)
+    
